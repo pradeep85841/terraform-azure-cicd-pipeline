4bcb6b3fa879b5eb74b5843c21eb0ab741faa3fb
pradeep85841 <97498198+pradeep85841@users.noreply.github.com>
2025-10-17 15:14:26 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,6 +11,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "b1f95535-100e-4e15-abff-3ca8bdfd9bb9"
-  tenant_id       = "96ca3f2a-4cd6-4d1e-bbf5-f4abb87339bd"
+  subscription_id = "xxx"
+  tenant_id       = "xxx"
 }
