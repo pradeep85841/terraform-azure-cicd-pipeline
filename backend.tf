bc6490ca19474a7613239286c54d159382b5a717
pradeep85841 <97498198+pradeep85841@users.noreply.github.com>
2025-10-17 15:15:02 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-dev-iac"
-    storage_account_name = "terraformstate85841" # must be unique globally
+    storage_account_name = "xxx" # must be unique globally
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
